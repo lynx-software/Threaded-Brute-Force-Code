9105bb07ad465026d1dbdd616b8c819cce43e999
Lynx <mm193964@pennwest.edu>
2023-04-24 15:23:16 -0400

    new functions

--- searcher.cpp
+++ searcher.cpp
@@ -5,11 +5,13 @@
 */
 
 #include <iostream>
-#include <thread>
+#include <crypt.h> // encryption
+// #include <thread>
 
 using namespace std;
 
 const bool DEBUG_MODE = true;
+const string encryptedPassword = "DBtH8T8yNcN0U";
 const char CHARACTER_SET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 const int MAX_CHARACTERS = 6;
 
@@ -19,6 +21,7 @@ struct SearchSpaceT {
 };
 
 int GetCmdLineArgs(int argc, char * argv[]);
+string IntArrayToString(int indexArray[]);
 string BruteForcePassword(SearchSpaceT search);
 
 int main(int argc, char * argv[]) {
@@ -36,6 +39,29 @@ int GetCmdLineArgs(int argc, char * argv[]) {
     exit(1);
 }
 
-string BruteForcePassword(SearchSpaceT search) {
+string IntArrayToString(int indexArray[]) {
+    string builtString;
 
+    for (int i = 0; i < MAX_CHARACTERS; i++) {
+        builtString = builtString + CHARACTER_SET[indexArray[i]];
+    }
+
+    return builtString;
+}
+
+string BruteForcePassword(SearchSpaceT search) {
+    int charIndexArray[MAX_CHARACTERS] = {search.startCharIndex, 0, 0, 0, 0, 0};
+    string stringFromArray;
+    string encryptedString;
+    string salt = "DB";
+    crypt_data data;
+
+    while (charIndexArray[0] <= search.endCharIndex) {
+        stringFromArray = IntArrayToString(charIndexArray);
+        encryptedString = crypt_r(stringFromArray.c_str(), salt.c_str(), data);
+        if (encryptedString == encryptedPassword) {
+            return stringFromArray;
+        }
+    }
+    return "";
 }
