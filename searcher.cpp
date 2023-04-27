9897d65820af019ffbc47382a281e12c10a34939
Lynx <mm193964@pennwest.edu>
2023-04-27 16:02:36 -0400

    it works now

--- searcher.cpp
+++ searcher.cpp
@@ -11,30 +11,47 @@
 #include <cassert>
 // #include <mutex>
 #include <vector>
+#include <fstream>
+// #include <sys/times.h>
 
 using namespace std;
 
 const bool DEBUG_MODE = true;
-const string encryptedPassword = "DBtH8T8yNcN0U";
-const char CHARACTER_SET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-const int MAX_CHARACTERS = 6;
+// const string encryptedPassword = "DBtH8T8yNcN0U";
+const string encryptedPassword = "DBDLjTPhWLYM2";
+const int CHARS_IN_SET = 26;
+const char CHARACTER_SET[CHARS_IN_SET] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+// const int MAX_CHARACTERS = 6;
+const int MAX_CHARACTERS = 5;
+
+string result = "";
+// mutex result_mutex;
 
 class SearchSpaceT {
     public:
         SearchSpaceT(int start, int end);
-        string BruteForcePassword();
+        void BruteForcePassword();
+        void IncrementArray(int (&array)[MAX_CHARACTERS]);
     private:
         int startCharIndex;
         int endCharIndex;
 };
 
+// class ClockT {
+//     public:
+//         ClockT();
+//         ~ClockT();
+//     private:
+// };
+
 int GetCmdLineArgs(int argc, char * argv[]);
 string IntArrayToString(int indexArray[]);
+void WriteResultToFile();
 
 int main(int argc, char * argv[]) {
     vector<thread> threads;
     int n = GetCmdLineArgs(argc, argv);
-    int searchSize = 26 / n;
+    int searchSize = CHARS_IN_SET / n;
     int searchBegin = 0;
     int searchEnd = searchSize;
     SearchSpaceT searchSpace(searchBegin, searchEnd);
@@ -46,10 +63,10 @@ int main(int argc, char * argv[]) {
 
         // make new search space
         searchBegin = searchEnd + 1;
-        if (searchBegin + searchSize < 26) {
+        if (searchBegin + searchSize < CHARS_IN_SET) {
             searchEnd = searchBegin + searchSize;
         } else {
-            searchEnd = 25;
+            searchEnd = CHARS_IN_SET - 1;
         }
         searchSpace = SearchSpaceT(searchBegin, searchEnd);
     }
@@ -57,6 +74,8 @@ int main(int argc, char * argv[]) {
     for (int i = 0; i < n; i++) {
        threads[i].join();
     }
+
+    WriteResultToFile();
 
     return 0;
 }
@@ -87,44 +106,74 @@ SearchSpaceT::SearchSpaceT(int start, int end) {
     return;
 }
 
-string SearchSpaceT::BruteForcePassword() {
-    int charIndexArray[MAX_CHARACTERS] = {startCharIndex, 0, 0, 0, 0, 0};
+void SearchSpaceT::BruteForcePassword() {
     string stringFromArray;
     string encryptedString;
-    string salt = "DB";
-    // int start = startCharIndex;
+    const string SALT = "DB";
     crypt_data data;
-    int i = MAX_CHARACTERS - 1;
-    bool incremented;
+    int charIndexArray[MAX_CHARACTERS];
+
+    charIndexArray[0] = startCharIndex;
+    for (int i = 1; i < MAX_CHARACTERS; i++) {
+        charIndexArray[i] = 0;
+    }
 
     data.initialized = 0;
 
-    while (charIndexArray[0] <= endCharIndex) {
+    while (charIndexArray[0] <= endCharIndex and result == "") {
         stringFromArray = IntArrayToString(charIndexArray);
-        crypt_r(stringFromArray.c_str(), salt.c_str(), &data);
+        crypt_r(stringFromArray.c_str(), SALT.c_str(), &data);
         encryptedString = data.output;
+        if (DEBUG_MODE) {
+            cout << "stringFromArray = " << stringFromArray << "\tencryptedString = " << encryptedString << endl;
+        }
 
         if (encryptedString == encryptedPassword) {
-            return stringFromArray;
-        }
-        
-        // increment search
-        if (charIndexArray[MAX_CHARACTERS - 1] == 25) {
-            while (i >= 0 and !incremented) {
-                if (charIndexArray[i] != 25) {
-                    charIndexArray[i] += 1;
-                    incremented = true;
-                }
-                i--;
-            }
-            // max search value reached
-            if (!incremented) {
-                charIndexArray[0] = endCharIndex + 1;
-            }
+            // result_mutex.lock();
+            result = stringFromArray;
+            // result_mutex.unlock();
+            charIndexArray[0] = endCharIndex + 1;
         } else {
-            charIndexArray[MAX_CHARACTERS - 1] += 1;
+            IncrementArray(charIndexArray);
         }
     }
+}
 
-    return "";
+void SearchSpaceT::IncrementArray(int (&array)[MAX_CHARACTERS]) {
+    bool incremented = false;
+    int i = MAX_CHARACTERS - 1;
+    const int MAXVALUE = CHARS_IN_SET - 1;
+
+    while (i >= 0 and !incremented) {
+        if (array[i] != MAXVALUE) {
+            array[i] += 1;
+            incremented = true;
+            for (int j = MAX_CHARACTERS - 1; j > i; j--) {
+                if (array[j] == MAXVALUE) {
+                    array[j] = 0;
+                }
+            }
+        }
+        i--;
+    }
+
+    if (!incremented) {
+        array[0] = endCharIndex + 1;
+    }
+
+    return;
+}
+
+void WriteResultToFile() {
+    assert(result != "");
+
+    ofstream outFile("output.txt");
+
+    if (DEBUG_MODE) {
+        cout << "--- RESULT: " << result << " ---" << endl;
+    }
+    outFile << result << '\n';
+
+    outFile.close();
+    return;
 }
